9ab764ed3d1e658bbbbb62e08d8ad03638e0d8e2
prasunpatidar94 <prasunpatidar94@gmail.com>
2021-02-18 15:37:35 +0530

    ubountu

---
+++ Number/js/main1.js
@@ -0,0 +1,14 @@
+// function work(data) {
+
+//     var number1 = 1000.555;
+
+
+
+// }
+
+// document.getElementById("demo").innerHTML = 5 + 6;
+document.getElementById("demo").innerHTML = 6 + 12;
+// let data = "prasun is good guy";
+
+console.log( document.getElementById("demo").);
+
